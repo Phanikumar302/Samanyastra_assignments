88d38fbbdac8d275ac3ac9550746bec95c3ec200
Phanikumar302 <phanikumar5556@gmail.com>
2025-07-07 16:59:59 +0530

    first commit

---
+++ Odd_or_even.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a Number:"))
+if num % 2 == 0:                           
+    print("Given number is Even")
+else:                                      
+    print("Given number is Odd")  
+
+print("hello world")      
